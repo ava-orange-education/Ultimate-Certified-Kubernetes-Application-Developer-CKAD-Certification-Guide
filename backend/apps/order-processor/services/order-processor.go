2ba46caaf4c9b211c53871bec9c5399471b3fef3
hiteshrepo <pattanayak.hitesh03@gmail.com>
2024-12-31 16:56:16 +0530

    simulates batch processing

--- backend/apps/order-processor/services/order-processor.go
+++ backend/apps/order-processor/services/order-processor.go
@@ -23,9 +23,13 @@ func (ops *OrderProcessingService) AddRoutes() *chi.Mux {
 	router.Use(middleware.Logger)
 	router.Use(middleware.Recoverer)
 
+	// API routes
 	router.Route("/orders", func(r chi.Router) {
 		r.Post("/create", ops.oph.CreateOrder)
 		r.Put("/update-status", ops.oph.UpdateOrderStatus)
+
+		// Batch processing endpoint for Jobs/CronJobs
+		r.Post("/batch-process", ops.oph.BatchProcessHandler)
 	})
 
 	return router
